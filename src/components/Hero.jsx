e73e9b0a744d5008c1437dd2aa371b824db9be67
Acrazie <mayeul.desbazeille@epitech.eu>
2024-09-29 00:18:36 +0200

    Adding wrapper, animation, about section, mapping elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Salut ! Je m'appelle <span className="text-[#915eff]">Mayeul</span>
+            Salut ! Moi c'est <span className="text-[#915eff]">Mayeul</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Je suis un junior développeur web en Pre-Msc à Epitech Lille{" "}
@@ -24,6 +24,24 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev 
+            animate={{
+              y: [0, 24, 0]
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: 'loop'
+            }}
+            className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+
+      </div>
     </section>
   );
 };
